1375454f9f332750cbbd67d8e066329382f63784
PatStiles <33334338+PatStiles@users.noreply.github.com>
2018-04-13 15:42:03 -0500

    Add files via upload

--- GraphProcessor.java
+++ GraphProcessor.java
@@ -1,43 +1,10 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.awt.List;
+import java.util.stream.Collectors;
 
-/**
- * This class adds additional functionality to the graph as a whole.
- * 
- * Contains an instance variable, {@link #graph}, which stores information for all the vertices and edges.
- * @see #populateGraph(String)
- *  - loads a dictionary of words as vertices in the graph.
- *  - finds possible edges between all pairs of vertices and adds these edges in the graph.
- *  - returns number of vertices added as Integer.
- *  - every call to this method will add to the existing graph.
- *  - this method needs to be invoked first for other methods on shortest path computation to work.
- * @see #shortestPathPrecomputation()
- *  - applies a shortest path algorithm to precompute data structures (that store shortest path data)
- *  - the shortest path data structures are used later to 
- *    to quickly find the shortest path and distance between two vertices.
- *  - this method is called after any call to populateGraph.
- *  - It is not called again unless new graph information is added via populateGraph().
- * @see #getShortestPath(String, String)
- *  - returns a list of vertices that constitute the shortest path between two given vertices, 
- *    computed using the precomputed data structures computed as part of {@link #shortestPathPrecomputation()}.
- *  - {@link #shortestPathPrecomputation()} must have been invoked once before invoking this method.
- * @see #getShortestDistance(String, String)
- *  - returns distance (number of edges) as an Integer for the shortest path between two given vertices
- *  - this is computed using the precomputed data structures computed as part of {@link #shortestPathPrecomputation()}.
- *  - {@link #shortestPathPrecomputation()} must have been invoked once before invoking this method.
- *  
- * @author sapan (sapan@cs.wisc.edu)
- * 
- */
 public class GraphProcessor {
-
-    /**
+	private GraphADT<String> graph;
+	
+	 /**
      * Graph which stores the dictionary words and their associated connections
      */
     private GraphADT<String> graph;
@@ -45,11 +12,12 @@ public class GraphProcessor {
     /**
      * Constructor for this class. Initializes instances variables to set the starting state of the object
      */
-    public GraphProcessor() {
-        this.graph = new Graph<>();
-    }
-        
-    /**
+	public GraphProcessor() 
+	{
+		this.graph = new Graph<>();
+	}
+	        
+	 /**
      * Builds a graph from the words in a file. Populate an internal graph, by adding words from the dictionary as vertices
      * and finding and adding the corresponding connections (edges) between 
      * existing words.
@@ -59,17 +27,38 @@ public class GraphProcessor {
      * 
      * For all possible pairs of vertices, finds if the pair of vertices is adjacent {@link WordProcessor#isAdjacent(String, String)}
      * If a pair is adjacent, adds an undirected and unweighted edge between the pair of vertices in the graph.
+     *
+     * Log any issues encountered (print the issue details)
      * 
      * @param filepath file path to the dictionary
-     * @return Integer the number of vertices (words) added
+     * @return Integer the number of vertices (words) added; return -1 if file not found or if encountering other exceptions
      */
-    public Integer populateGraph(String filepath) {
-        return 0;
-    
-    }
-
-    
-    /**
+	public Integer populate(String filepath)
+	{
+		
+		graph = new GraphADT<String>();
+		try {
+		List words = WordProcessor.getWordStream(filepath).collect(Collectors.toList());
+		} catch(Exception e) {
+			return -1;
+		}
+		Integer count;
+		for(String w : words)
+		{
+			graph.addVertex(w);
+			for(String s : words)
+			{
+				if(WordProcessor.isAdjacent(w,s))
+					graph.addEdge(w,s);
+			}
+			count++;
+		}
+		
+		shortestPathPrecomputation();
+		return count;
+	}
+	
+	/**
      * Gets the list of words that create the shortest path between word1 and word2
      * 
      * Example: Given a dictionary,
@@ -81,12 +70,15 @@ public class GraphProcessor {
      *             kit
      *  shortest path between cat and wheat is the following list of words:
      *     [cat, hat, heat, wheat]
+     *
+     * If word1 = word2, List will be empty. 
+     * Both the arguments will always be present in the graph.
      * 
      * @param word1 first word
      * @param word2 second word
      * @return List<String> list of the words
      */
-    public List<String> getShortestPath(String word1, String word2) {
+    public List getShortestPath(String word1, String word2) {
         return null;
     
     }
@@ -103,6 +95,9 @@ public class GraphProcessor {
      *             kit
      *  distance of the shortest path between cat and wheat, [cat, hat, heat, wheat]
      *   = 3 (the number of edges in the shortest path)
+     *
+     * Distance = -1 if no path found between words (true also for word1=word2)
+     * Both the arguments will always be present in the graph.
      * 
      * @param word1 first word
      * @param word2 second word
@@ -120,4 +115,7 @@ public class GraphProcessor {
     public void shortestPathPrecomputation() {
     
     }
+	
+	
+	
 }
